22a49fc58c444d3d9cab0c83e16e3473c248fdbe
JSH99 <tjgus9966@naver.com>
2022-08-18 22:43:06 +0900

    merge

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,63 @@
 // 메인 페이지 - 포디
-import React, { Component } from 'react';
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <h1>메인 페이지</h1>
-            </div>
-        );
-    }
+import React from 'react';
+import styled from 'styled-components';
+import ModelBox from '../components/ModelBox';
+import SlideShow from '../components/SlideShow';
+import { favoriteModelsDummy } from './DummyData';
+import { newModelsDummy } from './DummyData';
+
+export default function Main() {
+
+    return (
+        <div>
+            <SlideShow/>
+            <TitleText>Favorite Model</TitleText>
+            <MoreButton>더보기</MoreButton>
+            <ModelBoxDiv>
+                {favoriteModelsDummy.map((model, index) => (
+                    <ModelBox
+                        key={index}
+                        model_id={model.id}
+                        img_path={model.img_path}
+                        model_name={model.name}    
+                        model_price={model.price}
+                    />
+                ))}
+            </ModelBoxDiv>
+            <TitleText>New Model</TitleText>
+            <MoreButton>더보기</MoreButton>
+            <ModelBoxDiv>
+                {newModelsDummy.map((model, index) => (
+                    <ModelBox
+                        key={index}
+                        model_id={model.id}
+                        img_path={model.img_path}
+                        model_name={model.name}    
+                        model_price={model.price}
+                    />
+                ))}
+            </ModelBoxDiv>
+        </div>
+    );
 }
 
-export default Main;
+const TitleText = styled.div`
+    font-weight: 800;
+    font-size: 34px;
+    margin-left: 7.8rem;
+    margin-top: 2.5rem;
+`;
+
+const MoreButton = styled.button`
+    border: none;
+    margin-left: 78rem;
+    background-color: white;
+    font-size: 15px;
+    cursor: pointer;
+`;
+
+const ModelBoxDiv = styled.div`
+    width: 85%;
+    margin-left: 6.8rem;
+`;
